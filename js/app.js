bf172df9c28509245ee07dd4e9d8db4441dce250
bekzodsharipov <sizning_email@github.com>
2025-08-15 16:14:41 +0500

    first commit

--- js/app.js
+++ js/app.js
@@ -1,15 +1,46 @@
-const modal = document.querySelector('.modal');
-const openBtns = document.querySelectorAll('.hero__main-btn'); // Barcha tugmalarni olamiz
+document.addEventListener('DOMContentLoaded', () => {
+  const modal = document.querySelector('.modal');
+  const modalInputs = document.querySelectorAll('.modal__inp');
+  const joinBtn = document.querySelector('.modal__btn');
 
-openBtns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    modal.classList.add('active');
+  if (!modal) return;
+
+  document.body.addEventListener('click', (e) => {
+    if (e.target.closest('.hero__main-btn')) {
+      modal.classList.add('active');
+    } else if (e.target === modal) {
+      modal.classList.remove('active');
+    }
   });
-});
 
-// Modal fonini bosganda yopish
-modal.addEventListener('click', (e) => {
-  if (e.target === modal) {
-    modal.classList.remove('active');
-  }
+  joinBtn.addEventListener('click', (e) => {
+    let allFilled = true;
+
+    modalInputs.forEach(inp => {
+      const oldError = inp.parentElement.querySelector('.error');
+      if (oldError) oldError.remove();
+
+      let errorMsg = '';
+      if (inp.placeholder.includes('Ismingiz')) {
+        errorMsg = 'Iltimos, ismingizni kiriting';
+      } else if (inp.placeholder.includes('+998')) {
+        errorMsg = 'Iltimos, Malumotlarni kiriting ';
+      }
+
+      if (inp.value.trim() === '') {
+        allFilled = false;
+
+        const errorEl = document.createElement('div');
+        errorEl.classList.add('error');
+        errorEl.textContent = errorMsg;
+        inp.insertAdjacentElement('afterend', errorEl);
+      } else {
+        inp.style.border = '';
+      }
+    });
+
+    if (!allFilled) {
+      e.preventDefault(); 
+    }
+  });
 });
